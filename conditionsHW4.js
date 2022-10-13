4fa57578d56d3af5c10db2caa84d9abf8f5a14e7
irraaa <ra.da@live.com>
2022-10-13 13:44:07 +0200

    ispravka domaceg

--- conditionsHW4.js
+++ conditionsHW4.js
@@ -1,6 +1,15 @@
 //zadatak 4.
 
-var number = 7;
+var number = 6;
 var noun = "cat";
+var noun2 = "dog";
+var noun3 = "bird";
 
-console.log("I have " + number + " " + noun + "s" + "," + " and I like them better than " + "dogs" + " " + "or birds.");
+
+if(number<5){
+    console.log("I like " + noun2 + "s" + " and " + noun3 + "s" + "." );
+}else if(number==5){
+    console.log("I have" + " " + number + " "+ noun + "s" + ".")
+}else{
+    console.log("I like all animals.");
+}
