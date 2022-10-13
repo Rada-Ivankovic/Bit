ba62413bd7996cd5d0145333dbcfc6f544edfa57
irraaa <ra.da@live.com>
2022-10-13 17:50:35 +0200

    no message

---
+++ vezba9.js
@@ -0,0 +1,11 @@
+//zadatak 8.//
+function sumTriple(x, y) {
+  if (x == y) {
+    return 3 * (x + y);
+  }
+  else {
+    return (x + y);
+  }
+}
+console.log(sumTriple(12, 5));
+console.log(sumTriple(8, 8));
