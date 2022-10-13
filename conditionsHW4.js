188116b18a456d6a467bafa4e1199e5a3e2f242c
irraaa <ra.da@live.com>
2022-10-13 15:38:19 +0200

    izmenjena verzija zadatka 4.

--- conditionsHW4.js
+++ conditionsHW4.js
@@ -1,15 +1,19 @@
 //zadatak 4.
 
-var number = 6;
+var number = 5;
 var noun = "cat";
 var noun2 = "dog";
 var noun3 = "bird";
 
-
+function zadatak4(number){
 if(number<5){
-    console.log("I like " + noun2 + "s" + " and " + noun3 + "s" + "." );
+    return("I like " + noun2 + "s" + " and " + noun3 + "s" + "." );
 }else if(number==5){
-    console.log("I have" + " " + number + " "+ noun + "s" + ".")
+    return("I have" + " " + number + " "+ noun + "s" + ".")
 }else{
-    console.log("I like all animals.");
+    return("I like all animals.");
 }
+}
+console.log(zadatak4(4));
+console.log(zadatak4(5));
+console.log(zadatak4(6));
